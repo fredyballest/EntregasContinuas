9d9ad9c1fe5251ac2ab0f6f9f559f517843b6ba7
Fredy Ballesteros <fredy.ballesteros@gmail.com>
2019-05-05 16:30:09 -0500

    Modifica Travis Otra Funcionalidad

--- prodjango/webplayground/profiles/tests.py
+++ prodjango/webplayground/profiles/tests.py
@@ -1,52 +1,16 @@
+import unittest
 from selenium import webdriver
-from django.test import LiveServerTestCase
-from django.contrib.auth.models import User
+from selenium.webdriver.common.keys import Keys
+import time
 
+class GrammarlyGeneralTest(unittest.TestCase):
 
-class AdminTestCase(LiveServerTestCase):
     def setUp(self):
-        User.objects.create_superuser(
-            username='admin',
-            password='admin',
-            email='admin@example.com'
-        )
-        self.selenium = webdriver.Firefox()
-        self.selenium.maximize_window()
-        super(AdminTestCase, self).setUp()
-
-    def tearDown(self):
-        # Call tearDown to close the web browser
-        self.selenium.quit()
-        super(AdminTestCase, self).tearDown()
-
-    def test_create_user(self):
-        """
-        Django admin create user test
-        This test will create a user in django admin and assert that
-        page is redirected to the new user change form.
-        """
-        # Open the django admin page.
-        # DjangoLiveServerTestCase provides a live server url attribute
-        # to access the base url in tests
-        self.selenium.get(
-            '%s%s' % (self.live_server_url,  "/admin/")
+        # for dev I'll use only visible browser instead of no-gui one.
+        # but for production, it will use the no-gui one.
+        # self.driver = webdriver.Firefox()
+        self.driver = webdriver.Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True}
         )
 
-        # Fill login information of admin
-        username = self.selenium.find_element_by_id("id_username")
-        username.send_keys("admin")
-        password = self.selenium.find_element_by_id("id_password")
-        password.send_keys("admin")
-
-        # Locate Login button and click it
-        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
-        self.selenium.get(
-            '%s%s' % (self.live_server_url, "/admin/auth/user/add/")
-        )
-
-        # Fill the create user form with username and password
-        self.selenium.find_element_by_id("id_username").send_keys("test")
-        self.selenium.find_element_by_id("id_password1").send_keys("test")
-        self.selenium.find_element_by_id("id_password2").send_keys("test")
-        self.selenium.find_element_by_id("user_form").submit()
-        self.assertIn("Change user", self.selenium.title)
