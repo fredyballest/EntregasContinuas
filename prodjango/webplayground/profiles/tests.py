2f1260e8479d728995e46268be6fb0801c0dc48a
Fredy Ballesteros <fredy.ballesteros@gmail.com>
2019-05-06 13:52:40 -0500

    Arregla Binari

--- prodjango/webplayground/profiles/tests.py
+++ prodjango/webplayground/profiles/tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.firefox.options import Options
 options = Options();
 options.add_argument("--headless");
 
-driver = webdriver.Firefox(firefox_options=options,firefox_binary=binary);
+driver = webdriver.Firefox(firefox_options=options);
 
 driver.get("http://www.google.com/");
 driver.quit();
