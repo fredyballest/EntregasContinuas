dd27997f584ba085dd14947c6b54b2915de851f6
Fredy Ballesteros <fredy.ballesteros@gmail.com>
2019-05-06 17:00:44 -0500

    Prueba Simple Sin Selenium

--- prodjango/webplayground/profiles/tests.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
-
-options = Options()
-options.add_argument('-headless')
-firefox = Firefox(firefox_options=options)
